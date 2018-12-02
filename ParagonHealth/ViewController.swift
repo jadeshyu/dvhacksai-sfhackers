0c48b27faf0356a577866c0323241e03f8dfe348
Dmytro <dim.malashenko@gmai.com>
2018-12-01 17:59:01 -0800

    Added iOS project

---
+++ ParagonHealth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParagonHealth
+//
+//  Created by otto on 12/1/18.
+//  Copyright © 2018 ParagonHealth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
